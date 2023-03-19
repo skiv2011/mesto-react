6f5389407fe7c8d5a0ed2cb3494e0afeb695db3c
Pavel Zhukovskii <FiremanDzen@yandex.ru>
2023-03-19 22:47:48 +0300

    1

--- src/utils/api.js
+++ src/utils/api.js
@@ -93,10 +93,11 @@
   }
 }
 
-export const api = new Api({
+const api = new Api({
   baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-58',
   headers: {
     authorization: 'd2d0f131-e0ef-4ded-a353-fd5842dcb33b',
     'Content-Type': 'application/json'
   }
 });
+export default api;
